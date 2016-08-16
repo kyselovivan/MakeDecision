24d98590ac1d11352aa3bbef63b522c014eec83b
Ivan Kyselov <kyselovivan@gmail.com>
2016-08-16 16:41:47 +0300

    implemented Decision.class

--- app/src/main/java/com/ivart/makedecision/Model/Decision.java
+++ app/src/main/java/com/ivart/makedecision/Model/Decision.java
@@ -1,8 +1,44 @@
 package com.ivart.makedecision.Model;
 
+import com.orm.SugarRecord;
+import com.orm.dsl.Table;
+
 /**
  * Created by Ivan on 8/16/2016.
  */
 
-public class Decision {
+public class Decision extends SugarRecord {
+    @Table
+    private Long id;
+    public String mDecisionName;
+
+    public Decision() {
+    }
+
+    public Decision(String mDecisionName) {
+
+        this.mDecisionName = mDecisionName;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public String getmDecisionName() {
+
+        return mDecisionName;
+    }
+
+    public void setmDecisionName(String mDecisionName) {
+        this.mDecisionName = mDecisionName;
+    }
+
+    @Override
+    public String toString() {
+        return "Decision{" +
+                "id=" + id +
+                ", mDecisionName='" + mDecisionName + '\'' +
+                '}';
+    }
 }
