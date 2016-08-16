eabcaefc9f66443b2a90f2b02c2741f6c5b596e2
Ivan Kyselov <kyselovivan@gmail.com>
2016-08-16 15:57:22 +0300

    add package Activities

---
+++ app/src/main/java/com/ivart/makedecision/Activities/MakeDecisionActivity.java
@@ -0,0 +1,15 @@
+package com.ivart.makedecision.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ivart.makedecision.R;
+
+public class MakeDecisionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_decision);
+    }
+}
---
+++ app/src/main/java/com/ivart/makedecision/Activities/MysDecisionsActivity.java
@@ -0,0 +1,7 @@
+package com.ivart.makedecision.Activities;
+
+/**
+ * Created by Ivan on 8/16/2016.
+ */
+public class MysDecisionsActivity {
+}
