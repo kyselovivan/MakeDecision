9d00be9ac0ad6a52fd92ea4d5ec0fdbd8eb58dac
Ivan Kyselov <kyselovivan@gmail.com>
2016-08-16 15:35:23 +0300

    removed Main

--- app/src/main/java/com/ivart/makedecision/MainActivity.java
+++ app/src/main/java/com/ivart/makedecision/MainActivity.java
@@ -1,10 +1,8 @@
-package com.ivart.makedecision.activity;
+package com.ivart.makedecision;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
-
-import com.ivart.makedecision.R;
 
 public class MainActivity extends AppCompatActivity {
 
