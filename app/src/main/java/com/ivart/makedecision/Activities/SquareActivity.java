65690c77ac54793e6398f1fec5e48e9146291926
Ivan Kyselov <kyselovivan@gmail.com>
2016-08-22 17:09:45 +0300

    SquareActivity added

---
+++ app/src/main/java/com/ivart/makedecision/Activities/SquareActivity.java
@@ -0,0 +1,15 @@
+package com.ivart.makedecision.Activities;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ivart.makedecision.R;
+
+public class SquareActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_square);
+    }
+}
