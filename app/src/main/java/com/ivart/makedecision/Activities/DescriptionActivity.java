6ebf0bb604a5287554e09213f51c09b6f6039f16
Ivan Kyselov <kyselovivan@gmail.com>
2016-08-23 11:30:07 +0300

    description text validation added

--- app/src/main/java/com/ivart/makedecision/Activities/DescriptionActivity.java
+++ app/src/main/java/com/ivart/makedecision/Activities/DescriptionActivity.java
@@ -16,7 +16,7 @@ import com.ivart.makedecision.R;
 
 import io.realm.Realm;
 
-public class DescriptionActivity extends AppCompatActivity implements View.OnClickListener{
+public class DescriptionActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText description;
     Button addDescription;
@@ -30,11 +30,11 @@ public class DescriptionActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
         Intent intent = getIntent();
-        decisionId = intent.getLongExtra("idDecision",0L);
+        decisionId = intent.getLongExtra("idDecision", 0L);
         realm = Realm.getDefaultInstance();
-        square = intent.getIntExtra("squareNumber",0);
-        description = (EditText)findViewById(R.id.edt_decision_description);
-        addDescription = (Button)findViewById(R.id.btn_add_description);
+        square = intent.getIntExtra("squareNumber", 0);
+        description = (EditText) findViewById(R.id.edt_decision_description);
+        addDescription = (Button) findViewById(R.id.btn_add_description);
 
         setOnClick();
 
@@ -47,10 +47,12 @@ public class DescriptionActivity extends AppCompatActivity implements View.OnCli
     @Override
     public void onClick(View v) {
         String text = description.getText().toString();
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_add_description:
-                saveIntoDatabase(decisionId,square,text);
-                //Toast.makeText(this,"Decision id = " + decisionId + "\nSquare number = " + square, Toast.LENGTH_LONG).show();
+                if (text.equals("") || text == null) {
+                    Toast.makeText(this, R.string.please_enter_description, Toast.LENGTH_LONG).show();
+                } else
+                    saveIntoDatabase(decisionId, square, text);
                 break;
         }
     }
@@ -66,14 +68,14 @@ public class DescriptionActivity extends AppCompatActivity implements View.OnCli
                 decisionDescription.setSquare(square);
                 decisionDescription.setDescriptionText(descriptionText);
 
-                Log.d("LOG","<<<<<<<<<<<<<<< success >>>>>>>>>>>>>>>>\n" +
-                "Decision Id = "+ decisionId + "\n" +
-                "Decision Description Id = " + id + "\n" +
-                "Decision text = " + descriptionText + "\n" +
-                "Decision Square = " + square + "\n");
+                Log.d("LOG", "<<<<<<<<<<<<<<< success >>>>>>>>>>>>>>>>\n" +
+                        "Decision Id = " + decisionId + "\n" +
+                        "Decision Description Id = " + id + "\n" +
+                        "Decision text = " + descriptionText + "\n" +
+                        "Decision Square = " + square + "\n");
             }
         });
         description.getText().clear();
-        Toast.makeText(DescriptionActivity.this,R.string.description_was_added,Toast.LENGTH_LONG).show();
+        Toast.makeText(DescriptionActivity.this, R.string.description_was_added, Toast.LENGTH_LONG).show();
     }
 }
