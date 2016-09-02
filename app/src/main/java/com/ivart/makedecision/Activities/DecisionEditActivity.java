d5b44236158ef0bf127016d2b887e9508a7090c9
Ivan Kyselov <kyselovivan@gmail.com>
2016-09-02 11:35:52 +0300

    testing DecisionEditActivity start

--- app/src/main/java/com/ivart/makedecision/Activities/DecisionEditActivity.java
+++ app/src/main/java/com/ivart/makedecision/Activities/DecisionEditActivity.java
@@ -1,15 +1,33 @@
 package com.ivart.makedecision.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.ivart.makedecision.R;
 
 public class DecisionEditActivity extends AppCompatActivity {
 
+    Long decisionId;
+    Button firstSquare;
+    Button secondSquare;
+    Button thirdSquare;
+    Button fourthSquare;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_decision_edit);
+        setContentView(R.layout.activity_square);
+        Intent intent = getIntent();
+        decisionId = intent.getLongExtra("decisionId", 0);
+
+        firstSquare = (Button) findViewById(R.id.btn_will_if_it_happens);
+        secondSquare = (Button) findViewById(R.id.btn_will_if_it_doesnt);
+        thirdSquare = (Button) findViewById(R.id.btn_wont_if_it_happens);
+        fourthSquare = (Button) findViewById(R.id.btn_wont_if_it_doesnt);
+
+        Toast.makeText(this, ""+decisionId, Toast.LENGTH_SHORT).show();
     }
 }
