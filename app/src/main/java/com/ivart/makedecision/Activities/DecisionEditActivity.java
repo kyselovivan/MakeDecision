4e43eb26c7172795b8d05b727aa7fe92d35e7a02
Ivan Kyselov <kyselovivan@gmail.com>
2016-09-02 11:27:29 +0300

    DecisionEditActivity added

---
+++ app/src/main/java/com/ivart/makedecision/Activities/DecisionEditActivity.java
@@ -0,0 +1,13 @@
+package com.ivart.makedecision.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DecisionEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decision_edit);
+    }
+}
