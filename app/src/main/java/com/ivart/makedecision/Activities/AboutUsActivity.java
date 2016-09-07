31679dbe5190cb24c8defeb34f49ee5be5a99298
Ivan Kyselov <kyselovivan@gmail.com>
2016-09-07 15:27:22 +0300

    testing calculate decisions

--- app/src/main/java/com/ivart/makedecision/Activities/AboutUsActivity.java
+++ app/src/main/java/com/ivart/makedecision/Activities/AboutUsActivity.java
@@ -2,7 +2,9 @@ package com.ivart.makedecision.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.ivart.makedecision.Model.CalculateDecison;
 import com.ivart.makedecision.R;
 
 /**
@@ -10,8 +12,12 @@ import com.ivart.makedecision.R;
  */
 public class AboutUsActivity extends Activity{
 
+    CalculateDecison calculate;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        calculate = new CalculateDecison();
+        Toast.makeText(this,""+calculate.getSummaryRaiting(),Toast.LENGTH_LONG).show();
     }
 }
