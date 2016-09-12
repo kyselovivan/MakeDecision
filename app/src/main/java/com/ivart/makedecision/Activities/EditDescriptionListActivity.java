c33825976ed4884c8a10a64bcfa93a7d6714e928
Ivan Kyselov <kyselovivan@gmail.com>
2016-09-12 14:07:11 +0300

    added button Calculate

--- app/src/main/java/com/ivart/makedecision/Activities/EditDescriptionListActivity.java
+++ app/src/main/java/com/ivart/makedecision/Activities/EditDescriptionListActivity.java
@@ -5,6 +5,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -12,6 +14,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.ivart.makedecision.Adapters.DescriptionListAdapter;
+import com.ivart.makedecision.Model.CalculateDecison;
 import com.ivart.makedecision.Model.DecisionDescription;
 import com.ivart.makedecision.R;
 
@@ -157,6 +160,31 @@ public class EditDescriptionListActivity extends Activity  {
     protected void onRestart(){
         super.onRestart();
         descriptionsList.invalidateViews();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_action_bar, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.calculate) {
+            CalculateDecison calculate = new CalculateDecison();
+            double ifItHapp = calculate.getRaitingBySquare(decisionId,1);
+            double ifItDoesnt = calculate.getRaitingBySquare(decisionId,2);
+            double wontItItHapp = calculate.getRaitingBySquare(decisionId,3);
+            double wontItItDoesnt = calculate.getRaitingBySquare(decisionId,4);
+            double[] results = {ifItHapp,ifItDoesnt,wontItItHapp,wontItItDoesnt};
+            Intent intent = new Intent(this, PieChartActivity.class);
+            intent.putExtra("results",results);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
