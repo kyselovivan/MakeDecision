c430890b11cdaa7e685183b886dba8317a3e3d24
Ivan Kyselov <kyselovivan@gmail.com>
2016-09-23 14:12:03 +0300

    AboutTechnique activity added

---
+++ app/src/main/java/com/ivart/makedecision/Activities/AboutTechnique.java
@@ -0,0 +1,14 @@
+package com.ivart.makedecision.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ivart.makedecision.R;
+
+public class AboutTechnique extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_technique);
+    }
+}
