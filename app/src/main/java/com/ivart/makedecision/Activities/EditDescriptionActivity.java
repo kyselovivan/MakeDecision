fecec34bcafece0e8a83931b69bb7bf293bd1b9f
Ivan Kyselov <kyselovivan@gmail.com>
2016-09-26 12:15:59 +0300

    change rext selection while editing (after text)

--- app/src/main/java/com/ivart/makedecision/Activities/EditDescriptionActivity.java
+++ app/src/main/java/com/ivart/makedecision/Activities/EditDescriptionActivity.java
@@ -27,23 +27,27 @@ public class EditDescriptionActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_description);
         Intent intent = getIntent();
-        descriptionId = intent.getLongExtra("descriptionId",0);
+        descriptionId = intent.getLongExtra("descriptionId", 0);
         realm = Realm.getDefaultInstance();
 //        final RealmResults<DecisionDescription> results = realm.where(DecisionDescription.class)
 //                .equalTo("id",descriptionId)
 //                .findAll();
-        DecisionDescription textToEdit = realm.where(DecisionDescription.class).equalTo("id",descriptionId).findFirst();
+        DecisionDescription textToEdit = realm.where(DecisionDescription.class).equalTo("id", descriptionId).findFirst();
         String tempText = textToEdit.getDescriptionText();
-        editTextDescription = (EditText)findViewById(R.id.edt_edit_decision_description);
-        editRaitBar = (RatingBar)findViewById(R.id.edit_raiting_bar);
-        editTextDescription.setText(tempText);
+        editTextDescription = (EditText) findViewById(R.id.edt_edit_decision_description);
+        if (!tempText.isEmpty()) {
+            editTextDescription.setText(tempText);
+            editTextDescription.setSelection(tempText.length());
+        }
+        editRaitBar = (RatingBar) findViewById(R.id.edit_raiting_bar);
+
 
     }
 
 
-    public void updateDescription(Realm realm){
+    public void updateDescription(Realm realm) {
         DecisionDescription toEdit = realm.where(DecisionDescription.class)
-                .equalTo("id",descriptionId).findFirst();
+                .equalTo("id", descriptionId).findFirst();
         realm.beginTransaction();
         toEdit.setDescriptionText(editTextDescription.getText().toString());
         toEdit.setRaiting(editRaitBar.getRating());
