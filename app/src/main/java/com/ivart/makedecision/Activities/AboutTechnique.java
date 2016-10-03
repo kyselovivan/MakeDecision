8b2d8ee2c16c2d7cae51538241a4a4fa9a192f2e
ArtemMasterJava <aanohin1@gmail.com>
2016-10-03 13:42:11 +0300

    en strings about technique

--- app/src/main/java/com/ivart/makedecision/Activities/AboutTechnique.java
+++ app/src/main/java/com/ivart/makedecision/Activities/AboutTechnique.java
@@ -1,14 +1,20 @@
 package com.ivart.makedecision.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
 import com.ivart.makedecision.R;
 
 public class AboutTechnique extends AppCompatActivity {
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_technique);
+        mTextView = (TextView) findViewById(R.id.textView_about_technique);
+        mTextView.setMovementMethod(new ScrollingMovementMethod());
     }
 }
